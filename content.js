161c2107b889dcfbba1bbe42b013d6d3cb08a290
Matteo <matteomoro996@gmail.com>
2020-12-28 17:25:18 +0100

    Add files via upload
    added a div to check the price

--- content.js
+++ content.js
@@ -20,4 +20,30 @@ for(var i = 0; i < elem.length; i++){
     prices_arr[i] = parseFloat(price_str);
 }
 
-alert(total_calculator(prices_arr));
+//alert(total_calculator(prices_arr));
+
+
+total = total_calculator(prices_arr);
+// Setting up the view
+
+x_pos = 45;
+y_pos = 500;
+size = 20;
+
+var div = document.createElement('div'); 
+div.style.fontSize = size + 'px';
+div.style.position = "absolute";
+div.style.left = x_pos+'px';
+div.style.top = y_pos+'px'; //creating element
+div.textContent = 'TOTAL: ' + total; //adding text on the element 
+// Color gradient just for fun
+if (total < 500) {
+  div.style.textColor = 'green';
+}
+else if (total > 500 || total < 800){
+  div.style.textColor = 'orange';
+}
+else{
+  div.style.textColor = 'red';
+} 
+document.body.appendChild(div); //appending the element
